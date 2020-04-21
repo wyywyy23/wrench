fd2019268e0128228cd4ed2ffa663e7dfde4801f
Travis <travis@travis-ci.org>
2020-04-21 05:10:54 +0000

    Travis build 2209

--- 1.6-dev/developer/search/all_1.js
+++ 1.6-dev/developer/search/all_1.js
@@ -6,7 +6,7 @@ var searchData=
   ['batch_5fqueue_5fordering_5falgorithm',['BATCH_QUEUE_ORDERING_ALGORITHM',['../classwrench_1_1_batch_compute_service_property.html#a58c3a7a25ee192ff4761f6ed3cea4533',1,'wrench::BatchComputeServiceProperty']]],
   ['batch_5frjms_5fpadding_5fdelay',['BATCH_RJMS_PADDING_DELAY',['../classwrench_1_1_batch_compute_service_property.html#adcd8150f9959ce356635519f97b16fe4',1,'wrench::BatchComputeServiceProperty']]],
   ['batch_5fscheduling_5falgorithm',['BATCH_SCHEDULING_ALGORITHM',['../classwrench_1_1_batch_compute_service_property.html#a4e88135150aea2df7f815f7d751f03c7',1,'wrench::BatchComputeServiceProperty']]],
-  ['batchcomputeservice',['BatchComputeService',['../classwrench_1_1_batch_compute_service.html',1,'wrench::BatchComputeService'],['../classwrench_1_1_batch_compute_service.html#a49082131bf5eb088baaeb3b15b158052',1,'wrench::BatchComputeService::BatchComputeService()']]],
+  ['batchcomputeservice',['BatchComputeService',['../classwrench_1_1_batch_compute_service.html',1,'wrench::BatchComputeService'],['../classwrench_1_1_batch_compute_service.html#a11544f6fad77b499b63fc809c75d78f4',1,'wrench::BatchComputeService::BatchComputeService()']]],
   ['batchcomputeservicemessagepayload',['BatchComputeServiceMessagePayload',['../classwrench_1_1_batch_compute_service_message_payload.html',1,'wrench']]],
   ['batchcomputeserviceproperty',['BatchComputeServiceProperty',['../classwrench_1_1_batch_compute_service_property.html',1,'wrench']]],
   ['batsched_5fcontiguous_5fallocation',['BATSCHED_CONTIGUOUS_ALLOCATION',['../classwrench_1_1_batch_compute_service_property.html#aa3d144deb356f416880972388b948178',1,'wrench::BatchComputeServiceProperty']]],
--- 1.6-dev/user/search/all_2.js
+++ 1.6-dev/user/search/all_2.js
@@ -7,6 +7,7 @@ var searchData=
   ['computeservice',['ComputeService',['../classwrench_1_1_compute_service.html',1,'wrench']]],
   ['computeservicemessagepayload',['ComputeServiceMessagePayload',['../classwrench_1_1_compute_service_message_payload.html',1,'wrench']]],
   ['computeserviceproperty',['ComputeServiceProperty',['../classwrench_1_1_compute_service_property.html',1,'wrench']]],
+  ['counter',['counter',['../classwrench_1_1_simulation_timestamp_disk_read.html#a7cd2fb713a7682a67492a78dec246655',1,'wrench::SimulationTimestampDiskRead::counter()'],['../classwrench_1_1_simulation_timestamp_disk_write.html#a6f85990e99dc9eae93313715f3330310',1,'wrench::SimulationTimestampDiskWrite::counter()']]],
   ['create_5fvm_5fanswer_5fmessage_5fpayload',['CREATE_VM_ANSWER_MESSAGE_PAYLOAD',['../classwrench_1_1_cloud_compute_service_message_payload.html#a89ee9c09d630899720ed7b05d344d7a8',1,'wrench::CloudComputeServiceMessagePayload']]],
   ['create_5fvm_5frequest_5fmessage_5fpayload',['CREATE_VM_REQUEST_MESSAGE_PAYLOAD',['../classwrench_1_1_cloud_compute_service_message_payload.html#a8c63b9a0caa28f45f870e82a1de60cf2',1,'wrench::CloudComputeServiceMessagePayload']]],
   ['createexecutableworkflowfromjson',['createExecutableWorkflowFromJSON',['../classwrench_1_1_pegasus_workflow_parser.html#a67bbfda5088da383448a248da47e90e0',1,'wrench::PegasusWorkflowParser']]],
